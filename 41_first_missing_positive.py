4af3b5bbec7cdb6478bba73c64d92c59ddaa48c5
Red <aa840211aa@gmail.com>
2021-10-19 20:00:00 +0800

    Add files via upload

---
+++ 41_first_missing_positive.py
@@ -0,0 +1,18 @@
+class Solution:
+	def firstMissingPositive(self, nums):
+		maxPositive = len(nums)
+		nums = set(nums)
+		for i in range(1, maxPositive+1):
+			if i not in nums:
+				return i
+		return maxPositive + 1
+
+
+if __name__ == '__main__':
+	sol = Solution()
+	nums = [7,8,9,11,12]
+	result = sol.firstMissingPositive(nums)
+	print(result)
+	print('success')
+	print('time complexity: O(n)')
+	print('space complexity: O(n)')
